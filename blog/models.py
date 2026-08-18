78be2610e2fecf0a597bf452001f1dc6ccfa89b0
Francis Poliran <francispoliran437@gmail.com>
2026-08-19 01:48:44 +0800

    adding changes:models.py

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, 
+                              choices=Status.choices, 
+                              default=Status.DRAFT)
 
 
     class Meta:
